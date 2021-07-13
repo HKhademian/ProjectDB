003409c036681c0202a7330811703d8a2d3041a5
Mohammad Jahan Dide <m.jahandide00@gmail.com>
2021-07-13 18:42:35 +0430

    add Page for add Background,Accomplishment,Language and their Controller in Profile

---
+++ src/controller/AddSupportedLanguageController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AddSupportedLanguageController {
+
+    @FXML
+    private JFXButton cancelButton;
+
+    @FXML
+    private JFXButton addButton;
+
+    @FXML
+    private Label languageError;
+
+    @FXML
+    public void initialize(){
+        languageError.setVisible(false);
+
+        cancelButton.setOnAction(event -> cancelButton.getScene().getWindow().hide());
+        addButton.setOnAction(event -> saveBackground());
+    }
+
+    private void saveBackground(){
+        boolean valid = true;
+        //check language an add it
+        addButton.getScene().getWindow().hide();
+    }
+}
