106ab6c82e2a5d2de772c1a9df5919d888ed0e69
Hossain Khademian <hossain.khademian@gmail.com>
2021-07-14 19:39:58 +0430

    add register section

--- src/main/java/app/repository/UserRepository.java
+++ src/main/java/app/repository/UserRepository.java
@@ -35,8 +35,24 @@ public final class UserRepository extends _BaseRepository {
     });
   }
 
-  public static User register(String name, String username, String password) {
-    return null;
+  public static User register(String username, String password, String name, String email, String phone, String intro, String about, byte[] avatar, Date birthday, String location) {
+    final var QUERY = "INSERT INTO `User` VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, NULL, ?, ?);";
+    return connect(connection -> {
+      final var statement = connection.prepareStatement(QUERY);
+      statement.setString(1, username);
+      statement.setString(2, password);
+      statement.setString(3, name);
+      statement.setString(4, email);
+      statement.setString(5, phone);
+      statement.setString(6, intro);
+      statement.setString(7, about);
+      statement.setBytes(8, avatar);
+      statement.setLong(9, birthday.getTime());
+      statement.setString(10, location);
+      final var res = statement.executeUpdate();
+      if (res > 0) return login(username, password);
+      return null;
+    });
   }
 
   public static List<Article> getUserHome(int userId) {
