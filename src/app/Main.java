56c480221eaff69c5f70766fd10a970638880024
Mohammad Jahan Dide <m.jahandide00@gmail.com>
2021-07-15 14:52:01 +0430

    Update Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,22 +1,23 @@
 package app;
 
 import app.controller.ProfileController;
+import app.controller.SignupController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import app.controller.OpenWindow;
 
 public class Main extends Application {
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    if ("backend".equals(System.getenv().getOrDefault("dev.mode", null))) {
-      OpenWindow.openWindow("scene.fxml", null, "test javafx");
-    } else {
-      OpenWindow.openWindow("view/Profile.fxml", new ProfileController(), "Linkedin - Profile");
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        if ("backend".equals(System.getenv().getOrDefault("dev.mode", null))) {
+            OpenWindow.openWindow("scene.fxml", null, "test javafx");
+        } else {
+            OpenWindow.openWindow("view/Signup.fxml", new SignupController(), "Linkedin - Profile");
+        }
     }
-  }
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
