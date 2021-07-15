0d0816619b85018d7e6dde07b6687093ea3f5083
Mohammad Jahan Dide <m.jahandide00@gmail.com>
2021-07-15 09:17:51 +0430

    add AddArticle Controller for its page

---
+++ src/app/controller/AddArticleController.java
@@ -0,0 +1,67 @@
+package app.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
+public class CreateArticleController {
+
+    private int userId;
+
+    public CreateArticleController(int userId) {
+        this.userId = userId;
+    }
+
+    @FXML
+    private TextArea titleBox;
+
+    @FXML
+    private TextArea textBox;
+
+    @FXML
+    private Label chooseTypeLabel;
+
+    @FXML
+    private Label address;
+
+    @FXML
+    private Label titleError;
+
+    @FXML
+    private JFXButton cancelButton;
+
+    @FXML
+    private JFXButton postButton;
+
+    @FXML
+    public void initialize() {
+
+        postButton.setOnAction(event -> {post();});
+
+        cancelButton.setOnAction(event -> {cancel();});
+
+    }
+
+    private void cancel(){
+        cancelButton.getScene().getWindow().hide();
+    }
+
+    private void post(){
+        String title = titleBox.getText().replaceFirst("\\s++$", "");
+        if(title.length() == 0 ){
+            titleError.setText("Title must be write");
+        }
+        else{
+            //add to database
+            postButton.getScene().getWindow().hide();
+        }
+    }
+
+}
