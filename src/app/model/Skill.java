c1d608d93422e8dd0e784aeeb214f9e7e60269ed
Hossain Khademian <hossain.khademian@gmail.com>
2021-07-15 10:16:41 +0430

    complete and test SkillRepository

--- src/app/model/Skill.java
+++ src/app/model/Skill.java
@@ -1,6 +1,20 @@
 package app.model;
 
-public class Skill {
+import java.sql.ResultSet;
+
+public class Skill extends _BaseModel {
   public int skillId;
   public String title;
+
+  public Skill(int skillId, String title) {
+    this.skillId = skillId;
+    this.title = title;
+  }
+
+  public static Skill from(ResultSet res) {
+    return new Skill(
+      tryInt(res, "skillId", 0),
+      tryString(res, "title")
+    );
+  }
 }
--- src/app/repository/SkillRepository.java
+++ src/app/repository/SkillRepository.java
@@ -2,6 +2,9 @@ package app.repository;
 
 import app.model.Skill;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public final class SkillRepository extends _BaseRepository {
@@ -9,11 +12,35 @@ public final class SkillRepository extends _BaseRepository {
   }
 
   public static List<Skill> listSkills() {
-    return null;
+    final String SQL = "SELECT * from `Skill`;";
+    return connect(connection -> {
+      final PreparedStatement statement = connection.prepareStatement(SQL);
+      final ResultSet res = statement.executeQuery();
+      final List<Skill> result = new ArrayList<>();
+      while (res.next())
+        result.add(Skill.from(res));
+      return result;
+    });
+  }
+
+  public static Skill getSkill(int skillId) {
+    final String SQL = "SELECT * from `Skill` where `skillId`=?;";
+    return connect(connection -> {
+      final PreparedStatement statement = connection.prepareStatement(SQL);
+      final ResultSet res = statement.executeQuery();
+      return res.next() ? Skill.from(res) : null;
+    });
   }
 
   public static Skill addSkill(String title) {
-    return null;
+    final String SQL = "INSERT INTO `Skill` VALUES (null,?) RETURNING *;";
+    return connect(connection -> {
+      final PreparedStatement statement = connection.prepareStatement(SQL);
+      statement.setString(1, title);
+      final ResultSet res = statement.executeQuery();
+      return res.next() ? Skill.from(res) : null;
+    });
   }
+
 
 }
