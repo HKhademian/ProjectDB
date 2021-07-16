86115268b7ef0ee19ff9ef3cddaa8097bb089a18
Mohammad Jahan Dide <m.jahandide00@gmail.com>
2021-07-16 14:04:15 +0430

    update SignupController

--- src/app/controller/SignupController.java
+++ src/app/controller/SignupController.java
@@ -1,8 +1,8 @@
 package app.controller;
 
 import app.model.User;
-import app.repository.Repository;
 import com.jfoenix.controls.JFXComboBox;
+import app.repository.Repository;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import com.jfoenix.controls.JFXButton;
@@ -18,176 +18,173 @@ import java.util.Date;
 
 public class SignupController {
 
-  @FXML
-  private JFXButton loginButton;
+    @FXML
+    private JFXButton loginButton;
 
-  @FXML
-  private TextField usernameBox;
+    @FXML
+    private TextField usernameBox;
 
-  @FXML
-  private PasswordField passwordBox;
+    @FXML
+    private PasswordField passwordBox;
 
-  @FXML
-  private Label usernameError;
+    @FXML
+    private Label usernameError;
 
-  @FXML
-  private Label passwordError;
+    @FXML
+    private Label passwordError;
 
-  @FXML
-  private JFXCheckBox privacyBox;
+    @FXML
+    private JFXCheckBox privacyBox;
 
-  @FXML
-  private JFXButton signupButton;
+    @FXML
+    private JFXButton signupButton;
 
-  @FXML
-  private Label privacyError;
+    @FXML
+    private Label privacyError;
 
-  @FXML
-  private ImageView showPassword;
+    @FXML
+    private ImageView showPassword;
 
-  @FXML
-  private ImageView hidePassword;
+    @FXML
+    private ImageView hidePassword;
 
-  @FXML
-  private TextField passwordShowField;
+    @FXML
+    private TextField passwordShowField;
 
-  @FXML
-  private Label privacy;
+    @FXML
+    private Label privacy;
 
-  @FXML
-  private TextField nameBox;
+    @FXML
+    private TextField nameBox;
 
-  @FXML
-  private TextField familyBox;
+    @FXML
+    private TextField familyBox;
 
-  @FXML
-  private TextField birthdayBox;
+    @FXML
+    private TextField birthdayBox;
 
-  @FXML
-  private JFXComboBox<?> locationBox;
+    @FXML
+    private JFXComboBox<?> locationBox;
 
-  @FXML
-  private Label birthdayError;
+    @FXML
+    private Label birthdayError;
 
-  private Date birthday;
+    private Date birthday;
 
-  @FXML
-  public void initialize() {
-    passwordShowField.setVisible(false);
-    showPassword.setVisible(false);
+    @FXML
+    public void initialize(){
+        passwordShowField.setVisible(false);
+        showPassword.setVisible(false);
 
-    //ObservableList<?> locations = FXCollections.observableArrayList();
-    //locationBox.setItems(locations);
+        //ObservableList<?> locations = FXCollections.observableArrayList();
+        //locationBox.setItems(locations);
 
-    hidePassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> showPassword());
+        hidePassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> showPassword());
 
-    showPassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> hidePassword());
+        showPassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> hidePassword());
 
-    signupButton.setOnAction(event -> {
-      signUp();
-    });
+        signupButton.setOnAction(event -> { signUp(); });
 
-    loginButton.setOnAction(event -> {
-      login();
-    });
+        loginButton.setOnAction(event -> {login();});
 
-  }
-
-  private void showPassword() {
-    hidePassword.setVisible(false);
-    passwordShowField.setText(passwordBox.getText());
-    passwordShowField.setVisible(true);
-    showPassword.setVisible(true);
-  }
-
-  private void hidePassword() {
-    showPassword.setVisible(false);
-    hidePassword.setVisible(true);
-    passwordBox.setText(passwordShowField.getText());
-    passwordBox.setVisible(true);
-    passwordShowField.setVisible(false);
-  }
-
-  private void signUp() {
-
-    boolean valid = validation();
-
-    if (valid) {
-      String username = usernameBox.getText().trim();
-      String password = getPassword();
-      String name = nameBox.getText().trim();
-      String family = familyBox.getText().trim();
-
-      User user = Repository.registerUser(new User(0, username, name, family,
-        null, null, birthday, null), password);
-
-      if (user == null) {
-        usernameError.setText("this username is taken");
-        return;
-      }
-
-      signupButton.getScene().getWindow().hide();
-      OpenWindow.openWindow("view/Profile.fxml", new ProfileController(user, user), "Linkedin - Profile");
     }
 
-  }
-
-  private Date getBirthday() throws ParseException {
-    SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-    String dateInString = birthdayBox.getText().trim();
-    if (dateInString.isEmpty()) {
-      return null;
-    }
-    Date date = formatter.parse(dateInString);
-    return date;
-  }
-
-  private String getPassword() {
-    String password;
-    if (hidePassword.isVisible()) {
-      password = passwordBox.getText().replaceFirst("\\s++$", "");
-    } else {
-      password = passwordShowField.getText().replaceFirst("\\s++$", "");
-    }
-    return password;
-  }
-
-  private boolean validation() {
-    boolean valid = true;
-    usernameError.setText("");
-    passwordError.setText("");
-    privacyError.setText("");
-    birthdayError.setText("");
-
-    String username = usernameBox.getText().replaceFirst("\\s++$", "");
-    String password = getPassword();
-
-    if (username.isEmpty()) {
-      usernameError.setText("username can not be empty");
-      valid = false;
-    }
-    if (password.isEmpty()) {
-      passwordError.setText("password can not be empty");
-      valid = false;
+    private void showPassword(){
+        hidePassword.setVisible(false);
+        passwordShowField.setText(passwordBox.getText());
+        passwordShowField.setVisible(true);
+        showPassword.setVisible(true);
     }
 
-    if (!privacyBox.isSelected()) {
-      privacyError.setText("You must agree with our privacy and policy");
-      valid = false;
+    private void hidePassword(){
+        showPassword.setVisible(false);
+        hidePassword.setVisible(true);
+        passwordBox.setText(passwordShowField.getText());
+        passwordBox.setVisible(true);
+        passwordShowField.setVisible(false);
     }
-    try {
-      birthday = getBirthday();
-    } catch (ParseException e) {
-      birthdayError.setText("birthday in not valid");
-      valid = false;
-    }
-    return valid;
-  }
 
-  private void login() {
-    loginButton.getScene().getWindow().hide();
-    OpenWindow.openWindow("view/Login.fxml", new LoginController(), "Linkedin - Login Page");
-  }
+    private void signUp(){
+
+        boolean valid = validation();
+
+        if(valid) {
+            String username = usernameBox.getText().trim();
+            String password = getPassword();
+            String name = nameBox.getText().trim();
+            String family = familyBox.getText().trim();
+
+            User user = Repository.registerUser(new User(0, username, name, family,
+                    null, null, birthday, null), password);
+
+            if(user==null){
+                usernameError.setText("this username is taken");
+                return;
+            }
+
+            signupButton.getScene().getWindow().hide();
+            OpenWindow.openWindow("view/Profile.fxml", new ProfileController(user, user), "Linkedin - Profile");
+        }
+
+    }
+
+    private Date getBirthday() throws ParseException{
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        String dateInString = birthdayBox.getText().trim();
+        if(dateInString.isEmpty()){
+            return null;
+        }
+        Date date = formatter.parse(dateInString);
+        return date;
+    }
+
+    private String getPassword(){
+        String password;
+        if(hidePassword.isVisible()){
+            password = passwordBox.getText().replaceFirst("\\s++$", "");
+        }
+        else{
+            password = passwordShowField.getText().replaceFirst("\\s++$", "");
+        }
+        return password;
+    }
+
+    private boolean validation(){
+        boolean valid = true;
+        usernameError.setText("");
+        passwordError.setText("");
+        privacyError.setText("");
+        birthdayError.setText("");
+
+        String username = usernameBox.getText().replaceFirst("\\s++$", "");
+        String password = getPassword();
+
+        if(username.isEmpty()){
+            usernameError.setText("username can not be empty");
+            valid = false;
+        }
+        if(password.isEmpty()){
+            passwordError.setText("password can not be empty");
+            valid = false;
+        }
+
+        if(!privacyBox.isSelected()){
+            privacyError.setText("You must agree with our privacy and policy");
+            valid = false;
+        }
+        try {
+            birthday = getBirthday();
+        } catch (ParseException e) {
+            birthdayError.setText("birthday in not valid");
+            valid = false;
+        }
+        return valid;
+    }
+
+    private void login(){
+        loginButton.getScene().getWindow().hide();
+        OpenWindow.openWindow("view/Login.fxml",new LoginController(), "Linkedin - Login Page");
+    }
 
 
 }
